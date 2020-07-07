fafed7b56c66bcae524da822f4ae87a0ef82eb7a
sjones4215 <bluesfansince2013@gmail.com>
2020-07-06 19:10:25 -0500

    Initial commit

--- cars_ruby.rb
+++ cars_ruby.rb
@@ -1,2 +1,10 @@
 puts "List of cars"
-puts "hello world"
+puts "Enter in the make of the car"
+make=gets
+puts "Enter the model of the car"
+model=gets
+puts "Enter the color of the car"
+color=gets
+puts "Enter the year of the car"
+year=gets
+puts "#{make} #{model} #{color} #{year}"
