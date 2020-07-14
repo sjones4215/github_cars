dbf07a28954573f6f1bab381dab398a63682916e
sjones4215 <bluesfansince2013@gmail.com>
2020-07-14 09:33:27 -0500

    file delete

--- cars_ruby.rb
+++
@@ -1,8 +0,0 @@
-def ask_question(value)
-   puts"Enter information for #{value}"
-   gets.chomp
-end
-def depriciation(value)
-   value.to_f / 2
-end
-puts "#{ask_question("make").capitalize}, #{ask_question("model").capitalize}, esitimated at #{depriciation(ask_question("price")}"
